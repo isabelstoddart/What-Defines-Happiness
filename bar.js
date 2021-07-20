519c7748cc5755907c756b523e235102f35f2c75
Isabel Stoddart <47155912+isabelstoddart@users.noreply.github.com>
2021-07-19 20:50:07 -0400

    Update bar.js

--- bar.js
+++ bar.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 1350 - margin.left - margin.right,
+    width = 1500 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
